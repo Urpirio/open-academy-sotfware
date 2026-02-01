b41f5522c2ffbea26ef9df69b460c78bf65549e8
UrpirioDev <urpiriojunior@gmail.com>
2026-02-01 16:57:51 -0400

    Agregar el componente SectionHeroClase a la página En_Clase

--- app/clases/[clase_id]/page.tsx
+++ app/clases/[clase_id]/page.tsx
@@ -1,9 +1,9 @@
-
+import SectionHeroClase from "@/components/Components - Clases/[clase_id]/Section - HeroClase";
 
 export default function En_Clase() {
   return (
     <div>
-      
+      <SectionHeroClase />
     </div>
-  )
+  );
 }
