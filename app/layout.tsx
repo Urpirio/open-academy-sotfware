fad26865da6a21a6b0d022d2ec52b847d4488475
UrpirioDev <urpiriojunior@gmail.com>
2026-02-01 16:57:19 -0400

    Refactorizar el componente de diseño para incluir SidebarProvider y Toaster, mejorando la estructura de la interfaz de usuario

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
-import "./globals.css";
+import SideBarPanelControl from "@/components/Components - Globales/components/SideBar - PanelControl";
+import "../style/globals.css";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { Toaster } from "@/components/ui/sonner";
+import Header from "@/components/Components - Globales/components/Header";
 
 export default function RootLayout({
   children,
@@ -7,7 +11,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-white">
+        <SidebarProvider>
+          <SideBarPanelControl />
+          <main className="w-full">
+           
+            {children}
+          </main>
+        </SidebarProvider>
+        <Toaster position="top-center" />
+      </body>
     </html>
   );
 }
