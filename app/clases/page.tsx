6e8ebe5346030c5fd6b4adf42b47e477235bb15c
UrpirioDev <urpiriojunior@gmail.com>
2026-02-01 16:57:42 -0400

    Refactor Clase component to render SectionSearch and SectionClasesList

--- app/clases/page.tsx
+++ app/clases/page.tsx
@@ -1,8 +1,11 @@
+import SectionClasesList from "@/components/Components - Clases/clases/Section - ClasesList";
+import SectionSearch from "@/components/Components - Clases/clases/Section - Search";
 
 export default function Clase() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <SectionSearch />
+      <SectionClasesList />
+    </>
+  );
 }
